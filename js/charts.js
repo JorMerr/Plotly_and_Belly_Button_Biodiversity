aafe08b3785f5d5d5932a8f534ab105dbed2c857
cookandcrafter <98176999+JorMerr@users.noreply.github.com>
2022-05-19 13:14:23 -0400

    Deliverable 3 completed

--- js/charts.js
+++ js/charts.js
@@ -68,8 +68,6 @@ function buildCharts(sample) {
     var otu_labels = [result.otu_labels];
     var sample_values = [result.sample_values];
 
-
-
     // || --------- BAR CHART --------- ||
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
@@ -118,7 +116,45 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
-  });
-}
 
+      // || --------- Guage Chart --------- ||
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var metaID = data.metadata.filter(sampleObj => sampleObj.id == sample)[0];
+    // 3. Create a variable that holds the washing frequency.
+    var wFreq = parseFloat(metaID.wfreq);
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [{
+      domain: { x: [0,1], y:[0,1] },
+      value: wFreq,
+      title: { text: "Belly Button Washing Frequency<br><span style='color:gray'>Scrubs per Week</span>" },
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: {
+        axis: { 
+          range: [null, 10], 
+          tickvals: [0, 2, 4, 6, 8, 10]
+        },
+        bar: { color: "black" },
+        steps: [
+          { range: [0, 2], color: "red" },
+          { range: [2, 4], color: "orange" },
+          { range: [4, 6], color: "yellow" },
+          { range: [6, 8], color: "yellowgreen" },
+          { range: [8, 10], color: "darkgreen" }        
+        ]
+      }
+    }];
+  
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+    width: 450,
+    height: 450, 
+
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+  });
+};
 
